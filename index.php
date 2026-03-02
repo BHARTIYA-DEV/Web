55a7da1f28a23e9211ccce239b1cc29ed9b9865a
BHARTIYA-DEV <itzhimanshu200501@gmail.com>
2026-03-02 16:25:38 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=\, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+  <header>
+    <nav class="navbar">
+
+
+      <div><a href=""></a>Home</div>
+      <div>Content</div>
+      <div>Contact Us</div>
+      <div>About</div>
+
+
+    </nav>
+
+
+  </header>
+  <main>
+
+    <div class="leftSection">
+
+
+      <h2>Category </h2>
+      <div class="categories">
+
+        <div>1.Furniture</div>
+        <div>2.Laptops</div>
+        <div>3.Kitchen Appliance</div>
+        <div>4.Accessories</div>
+        <div>5.Makeup Items</div>
+
+      </div>
+
+
+
+
+    </div>
+    <div class="rightSection">
+
+      <h2>Products</h2>
+
+
+    </div>
+
+
+
+
+  </main>
+
+  <footer>
+
+    Copyright &#169; | 2025
+
+
+  </footer>
+</body>
+
+</html>
